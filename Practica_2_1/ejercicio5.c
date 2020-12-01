6158e238a3ecb594becc2fb841a3191483f8b558
CarmenSerrano <cserra02@ucm.es>
2020-12-01 17:33:02 +0100

    Ejercicio 5
    Muestra la información del sistema madiante la llamada al sistema  de la función uname.

---
+++ Practica_2_1/ejercicio5.c
@@ -0,0 +1,24 @@
+#include <sys/utsname.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(){
+
+struct utsname buffer; 
+int e;
+	e = uname(&buffer);
+
+printf("sistema : %s\n", buffer.sysname);
+printf("nombre del host : %s\n", buffer.nodename);
+printf("release del SO  : %s\n", buffer.release);
+printf("version : %s\n", buffer.version);
+printf("maquina : %s\n", buffer.machine);
+//printf("nombre del dominio : %s\n", buffer.domainname);
+
+
+
+return 1;
+}
